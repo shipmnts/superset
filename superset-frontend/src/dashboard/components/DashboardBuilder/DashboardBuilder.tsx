1633627588aef98362b79b9156d41e57e5298bf4
Ekansh Gupta <ekanshgupta@Ekanshs-MacBook-Pro.local>
2024-12-08 13:42:29 +0530

    mobile view css customisations fixes (not final code)

--- superset-frontend/src/dashboard/components/DashboardBuilder/DashboardBuilder.tsx
+++ superset-frontend/src/dashboard/components/DashboardBuilder/DashboardBuilder.tsx
@@ -289,6 +289,7 @@ const DashboardContentWrapper = styled.div`
       }
       .grid-row .dragdroppable-object {
         width: 100% ;
+        max-width: none ;
       }
       .resizable-container div {
         width: 100% !important ; 
